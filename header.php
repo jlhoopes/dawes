e359e96a6156ed72205f41d236b3efdda1b897b2
Jason Hoopes <jason@jlhoopes.com>
2018-06-11 14:21:41 -0400

    Integration of full site

---
+++ header.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>J&L Professional Services LLC.</title>
+		<!--meta-->
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.2" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="keywords" content="Construction, Renovation" />
+		<meta name="description" content="Responsive Construction Renovation Template" />
+		<!--slider revolution-->
+		<link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css">
+		<!--style-->
+		<link href='//fonts.googleapis.com/css?family=Raleway:100,300,400,500,600,700,900' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="style/reset.css">
+		<link rel="stylesheet" type="text/css" href="style/superfish.css">
+		<link rel="stylesheet" type="text/css" href="style/prettyPhoto.css">
+		<link rel="stylesheet" type="text/css" href="style/jquery.qtip.css">
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<link rel="stylesheet" type="text/css" href="style/animations.css">
+		<link rel="stylesheet" type="text/css" href="style/responsive.css">
+		<link rel="stylesheet" type="text/css" href="style/odometer-theme-default.css">
+		<!--fonts-->
+		<link rel="stylesheet" type="text/css" href="fonts/streamline-small/styles.css">
+		<link rel="stylesheet" type="text/css" href="fonts/streamline-large/styles.css">
+		<link rel="stylesheet" type="text/css" href="fonts/template/styles.css">
+		<link rel="stylesheet" type="text/css" href="fonts/social/styles.css">
+		<link rel="stylesheet" type="text/css" href="style/site.css">
+		<link rel="shortcut icon" href="images/favicon.ico">
+		<link rel="stylesheet" href="https://i.icomoon.io/public/temp/599b2ae2c4/JLLogo/style.css">
+	</head>
+	<!--<body class="image-1">
+	<body class="image-1 overlay">
+	<body class="image-2">
+	<body class="image-2 overlay">
+	<body class="image-3">
+	<body class="image-3 overlay">
+	<body class="image-4">
+	<body class="image-4 overlay">
+	<body class="image-5">
+	<body class="image-5 overlay">
+	<body class="pattern-1">
+	<body class="pattern-2">
+	<body class="pattern-3">
+	<body class="pattern-4">
+	<body class="pattern-5">
+	<body class="pattern-6">
+	<body class="pattern-7">
+	<body class="pattern-8">
+	<body class="pattern-9">
+	<body class="pattern-10">-->
+	<body class="<?php echo (isset($_COOKIE['re_layout']) && $_COOKIE['re_layout']=="boxed" ? (isset($_COOKIE['re_layout_style']) && $_COOKIE['re_layout_style']!="" ? $_COOKIE['re_layout_style'] . ' ' . $_COOKIE['re_image_overlay'] : 'image-1 overlay') : ''); ?>">
+		<div class="site-container<?php echo ($_COOKIE['re_layout']=="boxed" ? ' boxed' : ''); ?>">
+		<!--<div class="site-container boxed">-->
+			<div class="header-top-bar-container clearfix">
+				<div class="header-top-bar">
+					<ul class="contact-details clearfix">
+						<li class="template-phone">
+							<a href="tel:+16144001996">+1(614) 400-1996</a>
+						</li>
+						<li class="template-mail">
+							<a href="mailto:inquiries@dawesitright.com">inquiries@dawesitright.com</a>
+						</li>
+						<li class="template-clock">
+							Mon - Fri: 08.00 - 17.00
+						</li>
+					</ul>
+					<div class="search-container">
+						<a class="template-search" href="#" title="Search"></a>
+						<form class="search">
+							<input type="text" name="s" placeholder="Search..." value="Search..." class="search-input hint">
+							<fieldset class="search-submit-container">
+								<span class="template-search"></span>
+								<input type="submit" class="search-submit" value="">
+							</fieldset>
+							<input type="hidden" name="page" value="search">
+						</form>
+					</div>
+					<ul class="social-icons">
+						<li>
+							<a target="_blank" href="http://facebook.com/jandlprofessionalservicesllc" class="social-facebook" title="facebook"></a>
+						</li>
+						<li>
+							<a target="_blank" href="https://twitter.com/jandlprofessionalservicesllc" class="social-twitter" title="twitter"></a>
+						</li>
+						<li>
+							<a href="https://pinterest.com/jandlprofessionalservicesllc/" class="social-pinterest" title="pinterest"></a>
+						</li>
+						<!--<li>
+							<a title="" href="#" class="social-google-plus"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-youtube"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-linkedin"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-pinterest"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-dribble"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-skype"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-tumblr"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-instagram"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-xing"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-foursquare"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-rss"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-behance"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-picasa"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-stumbleupon"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-vimeo"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-houzz"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-yelp"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-github"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-reddit"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-soundcloud"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-vk"></a>
+						</li>
+						<li>
+							<a title="" href="#" class="social-vine"></a>
+						</li>
+						-->
+					</ul>
+				</div>
+				<a href="#" class="header-toggle template-arrow-up"></a>
+			</div>
+			<div class="header-container<?php echo (isset($_COOKIE['re_menu_type']) ? ' ' . $_COOKIE['re_menu_type'] : '');?>">
+			<!--<div class="header-container sticky">-->
+				<div class="vertical-align-table column-1-1">
+					<div class="header clearfix">
+						<div class="logo vertical-align-cell">
+							<h1><a href="?page=home" title="J&L Professional Services">
+								<span class="icon-JLLogo"></span>
+								<span class="text-fallback">J&L </span><span class="logo-text">Professional<br>Services LLC.</span></a></h1>
+						</div>
+						<a href="#" class="mobile-menu-switch vertical-align-cell">
+							<span class="line"></span>
+							<span class="line"></span>
+							<span class="line"></span>
+						</a>
+						<div class="menu-container clearfix vertical-align-cell">
+							<?php
+							require_once('menu.php');
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
