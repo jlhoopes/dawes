b139724e47e6fbd776449943405e9b44afb1df79
Hoopes, Jason <jason@jlhoopes.com>
2018-06-11 08:57:52 -0400

    Integration of boilerplate

--- index.php
+++ index.php
@@ -1,1 +1,118 @@
-<?php include 'maintenance.html' ?>
+
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>J&L Professional Services LLC.</title>
+  <style media="screen">
+  /* Style the header */
+    #mainHeader {
+    padding: 10px 16px;
+    background: #555;
+    color: #f1f1f1;
+    }
+
+    /* Page content */
+    .content {
+    padding: 16px;
+    }
+
+    /* The sticky class is added to the header with JS when it reaches its scroll position */
+    .sticky {
+    position: fixed;
+    top: 0;
+    width: 100%
+    }
+
+    /* Add some top padding to the page content to prevent sudden quick movement (as the header gets a new position at the top of the page (position:fixed and top:0) */
+    .sticky + .content {
+    padding-top: 102px;
+    }
+  </style>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+  <span>Phone: xxx-xxx-xxxx | Email: jeff@dawesitright.org | Hours: Mon-Fri 7:00am-9:00pm EST</span>
+
+    <header id="mainHeader">
+        <h2>Main Header</h2>
+    </header>
+    <section>
+      <br>
+      <br>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus ex a lectus hendrerit ultricies. Mauris interdum consectetur pulvinar. Cras vel lectus varius, iaculis ante et, placerat quam. Fusce mollis pulvinar ex, quis elementum libero maximus in. Ut sagittis dolor fermentum, gravida ipsum eget, imperdiet est. Quisque in orci orci. Cras odio mi, facilisis sit amet viverra ut, consectetur sed purus. Integer fringilla auctor mattis. Fusce scelerisque ligula in dolor tincidunt gravida. Fusce eget sagittis erat, vel mollis velit. Curabitur nisl urna, dapibus sed tincidunt vel, egestas vel mi. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+
+Curabitur suscipit felis congue, malesuada eros vel, auctor eros. In mollis hendrerit nibh sed consectetur. Sed ut metus eu sem mollis fermentum. Curabitur sit amet dignissim diam. Quisque vel quam convallis, rhoncus dui non, tempor dolor. Cras vel auctor nisi. Vivamus lacus massa, dignissim nec iaculis at, pulvinar luctus nisl. Sed in mi libero. Ut a risus dui. Aliquam malesuada dolor arcu, nec mollis velit eleifend sit amet. Quisque vel metus ultrices, luctus metus scelerisque, dignissim nisl. Phasellus aliquam hendrerit risus ut porta. Curabitur sagittis placerat magna sed semper.
+
+Aenean semper mollis urna. Maecenas condimentum est arcu, vitae facilisis lorem malesuada sed. Praesent vitae porta massa. Praesent vitae nisl eget nisl venenatis varius. Duis dapibus tempor ipsum a placerat. Cras vel dui eget nulla pellentesque convallis et a risus. Nunc a gravida nunc. Quisque egestas feugiat eleifend.
+
+Phasellus consequat tempor eleifend. Mauris vel velit aliquam, tincidunt mi id, eleifend felis. Duis laoreet tortor ut pharetra tempor. Etiam scelerisque blandit rutrum. Quisque a felis gravida, vehicula libero vel, accumsan quam. Mauris tempor tellus et magna tempus dapibus. Mauris massa lorem, dignissim in euismod non, egestas quis urna. In eget placerat quam. Donec orci neque, lacinia quis est at, posuere varius neque. Curabitur lacus nisi, porta at tincidunt a, congue rhoncus urna. Curabitur iaculis id nulla ut venenatis. Etiam nec tempor nisi.
+
+Praesent dictum tincidunt nunc a efficitur. Duis finibus lorem nec dapibus fermentum. Nam a lobortis dui. Curabitur non felis leo. Morbi in pharetra tortor, malesuada efficitur nisl. Maecenas vulputate tristique purus, a egestas augue pharetra eget. Nulla feugiat, massa eu scelerisque tristique, tellus arcu aliquam turpis, quis elementum eros urna nec justo. Nunc non tellus leo. In laoreet quis lacus id scelerisque. Phasellus auctor arcu sed fringilla iaculis. Aenean condimentum vestibulum tellus, eget gravida nisl egestas rhoncus. Sed tincidunt, justo vel faucibus facilisis, quam magna dictum dolor, ut sodales nisi sem id dui. Etiam elit orci, rutrum quis porta id, venenatis eget lacus. Fusce hendrerit nisl sem, sed aliquam enim ultrices at. Suspendisse et turpis metus. Mauris fermentum est vitae lectus congue viverra.
+
+Integer orci tellus, feugiat ac lectus id, tempor tempor lorem. Curabitur porta mollis augue a aliquet. Integer magna dui, rhoncus elementum diam sit amet, imperdiet congue dui. Curabitur dictum elementum aliquam. Ut vitae odio imperdiet, interdum nisl vitae, vulputate nunc. Nulla facilisi. Quisque quis vehicula neque. Nullam quis mattis enim, tempor sodales nulla. Phasellus id metus lectus. Pellentesque sagittis orci et erat sollicitudin, eget dictum justo maximus. Sed at dui dictum, sodales massa vitae, ultrices felis. Quisque ut libero eu ante aliquam efficitur. Aliquam erat volutpat. Suspendisse vel urna massa. Mauris interdum ipsum non sem pretium, at tristique velit tempus.
+
+Mauris eget pharetra velit, at finibus felis. Duis pharetra feugiat porta. Nam quam mauris, interdum ac nisi in, tincidunt ornare sem. Ut euismod elit et est tempus pharetra. Nam convallis nisi dictum felis volutpat hendrerit. Integer sit amet sodales ipsum. Duis ac quam ac massa efficitur fringilla. Nam maximus mi quis eleifend accumsan. Vestibulum vel sapien placerat, cursus mauris ut, molestie lorem. Nulla sagittis egestas vehicula. Nullam leo mauris, pellentesque nec ante quis, accumsan venenatis sem. Integer at velit aliquam tortor auctor imperdiet ut ac sapien. Cras a egestas arcu. Donec nisi justo, pulvinar ut massa eget, molestie pellentesque eros.
+
+Aenean non sem tortor. Proin purus nibh, mollis non sagittis at, sollicitudin a erat. Vivamus elit leo, semper ut vulputate ullamcorper, semper vel elit. Nullam imperdiet ultrices neque. Nunc dolor odio, viverra et felis quis, ornare ornare sapien. Vestibulum sagittis arcu purus, at mollis urna posuere ac. Cras finibus tellus odio, semper maximus metus fringilla et. Aliquam in elit hendrerit, malesuada tortor id, laoreet libero. Pellentesque blandit erat sit amet libero dapibus, vitae ultrices nisi eleifend. Nunc non ipsum in justo egestas semper. In euismod, nunc nec euismod imperdiet, risus enim gravida nunc, eu volutpat libero lorem sed ante.
+
+Etiam nisl tellus, aliquet at metus non, interdum tempor felis. Phasellus a imperdiet urna. Mauris eu volutpat elit, eget vehicula nibh. Nulla convallis eros sit amet finibus ultricies. Maecenas ac ex tellus. Vestibulum odio quam, consequat vel dignissim nec, gravida vel lorem. Cras non elit non lorem ultrices malesuada sed quis dolor. Pellentesque placerat eu nibh id auctor. Quisque varius nunc neque, ut ultricies leo faucibus nec. Mauris sit amet ante sit amet velit consectetur iaculis. Donec rutrum congue metus, sodales auctor quam.
+
+Cras porta sit amet magna et laoreet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque porta urna porttitor felis gravida commodo. Sed metus velit, porta id lacus vel, elementum pharetra augue. Etiam lobortis erat sagittis, condimentum lorem in, interdum enim. Suspendisse orci metus, auctor in justo et, feugiat congue arcu. Quisque viverra euismod accumsan. Donec ut enim risus. Maecenas suscipit, nunc id ornare auctor, lectus nisi placerat lacus, ut tincidunt justo sapien ac metus. Cras consequat malesuada fringilla. Suspendisse consectetur risus et faucibus tempus. Sed laoreet aliquam maximus.
+      <br>
+      <br>
+
+    </section>
+    <?php //include 'maintenance.html' ?>
+    <script type="text/javascript">
+      // When the user scrolls the page, execute myFunction
+      window.onscroll = function() {headerPop()};
+
+      // Get the header
+      var header = document.getElementById("mainHeader");
+
+      // Get the offset position of the navbar
+      var sticky = header.offsetTop;
+
+      // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+      function headerPop() {
+        if (window.pageYOffset >= sticky) {
+          header.classList.add("sticky");
+        } else {
+          header.classList.remove("sticky");
+        }
+      }
+
+    </script>
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
